fccf5fe0bc56a013fe08eb610a26c9338a6c98cd
Anna-mayorova <anyutkamayorova@gmail.com>
2025-03-12 16:01:42 +0300

    master commit

--- 1lab/main.py
+++ 1lab/main.py
@@ -2,9 +2,15 @@
 def main():
     # Ваш код здесь
     branch3()
+    branch2()
     pass
+
 def branch3():
     print('branch3')
+
+def branch2():
+    print('branch2222222')
+
 if __name__ == "__main__":
     main()
 
